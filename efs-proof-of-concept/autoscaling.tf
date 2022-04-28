9e9f06da8c6cfad16c537f4fa6e4263748a35b5f
Martin Rosselle <rosselm@gmail.com>
2022-04-28 20:15:58 +0200

    switched to ELB health check

--- efs-proof-of-concept/autoscaling.tf
+++ efs-proof-of-concept/autoscaling.tf
@@ -6,7 +6,7 @@ resource "aws_autoscaling_group" "rsync" {
   max_size                  = 1
   min_size                  = 1
   desired_capacity          = 1
-  health_check_type         = "EC2"
+  health_check_type         = "ELB"
   health_check_grace_period = 300
   force_delete              = true
   vpc_zone_identifier       = local.subnet_ids
